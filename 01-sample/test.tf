6b95b964bc9b42f6321ec0f8faa79fb579a8a965
Venkat431 <venkat.aella@gmail.com>
2023-03-15 16:57:58 -0700

    test

--- 01-sample/test.tf
+++ 01-sample/test.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2" {
   ami = "ami-0089b8e98cd95257d"
   instance_type = "t3.nano"
-  vpc_security_group_ids = "[sg-055e9d45bc8da8a4d]"
+  vpc_security_group_ids = ["sg-055e9d45bc8da8a4d"]
   tags = {
     name = "test"
   }
