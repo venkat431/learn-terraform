9c257f04d9e097b1b59745c20ebeccb9e5d92ab1
Venkat431 <venkat.aella@gmail.com>
2023-03-15 17:21:30 -0700

    test

---
+++ 03-data/data.tf
@@ -0,0 +1,11 @@
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+output "ami_id" {
+  Name = data
+  value = data.aws_ami.ami.image_id
+  #value = data.aws_ami.ami.description
+}
