70a9863608caa8d85cded58600b2492b6af98f00
Venkat431 <venkat.aella@gmail.com>
2023-03-18 14:36:23 -0700

    test servers

--- roboshop-setup/ex1/setup.tf
+++ roboshop-setup/ex1/setup.tf
@@ -1,3 +1,4 @@
+#create frontend component
 resource "aws_instance" "frontend" {
   ami = "ami-0089b8e98cd95257d"
   instance_type = "t3.nano"
@@ -6,7 +7,15 @@ resource "aws_instance" "frontend" {
     Name = "frontend"
   }
 }
-
+#create Route 53 for frontend
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z08931683BP7DV5GJ0PAA"
+  name    = "frontend.devops-practice.tech"
+  type    = "A"
+  ttl     = 30
+  records = aws_instance.frontend.private_ip
+}
+#create mongodb component
 resource "aws_instance" "mongodb" {
   ami = "ami-0089b8e98cd95257d"
   instance_type = "t3.nano"
@@ -14,4 +23,12 @@ resource "aws_instance" "mongodb" {
   tags = {
     Name = "mongodb"
   }
+}
+#create route 53 for mongodb
+resource "aws_route53_record" "mongodb" {
+  zone_id = "Z08931683BP7DV5GJ0PAA"
+  name    = "mongodb.devops-practice.tech"
+  type    = "A"
+  ttl     = 30
+  records = aws_instance.mongodb.private_ip
 }
