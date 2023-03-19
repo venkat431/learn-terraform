2b5535f64fa2ba80664f9c88ad1dcb58c2c0c3a3
Venkat431 <venkat.aella@gmail.com>
2023-03-18 17:21:15 -0700

    ex2

--- roboshop-setup/ex2/main.tf
+++ roboshop-setup/ex2/main.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "frontend" {
   instance_type          = "t3.nano"
   vpc_security_group_ids = ["sg-055e9d45bc8da8a4d"]
   tags                   = {
-    Name = "frontend"
+    Name = var.instances[count.index]
   }
 }
 
