84d71285ec5a8a6e184b97dc46645ad20b2ee473
Venkat431 <venkat.aella@gmail.com>
2023-03-18 17:58:26 -0700

    ex3

--- roboshop-setup/ex3/main.tf
+++ roboshop-setup/ex3/main.tf
@@ -6,7 +6,7 @@ data "aws_ami" "ami" {
 
 #create frontend component
 resource "aws_instance" "instance" {
-  for_each                  = var.instances
+  for_each               = var.instances
   ami                    = "ami-0089b8e98cd95257d"
   instance_type          = each.value["type"]
   vpc_security_group_ids = ["sg-055e9d45bc8da8a4d"]
