04f03ea2f19da4fb3d4670167bd674d5f32c6ed7
Venkat431 <venkat.aella@gmail.com>
2023-03-19 16:16:34 -0700

    sg

--- roboshop-setup/ex4/ec2/main.tf
+++ roboshop-setup/ex4/ec2/main.tf
@@ -8,7 +8,7 @@ data "aws_ami" "ami" {
 resource "aws_instance" "ec2" {
   ami = data.aws_ami.ami.image_id
   instance_type = var.instance_type
-  vpc_security_group_ids = var.sg_id
+  vpc_security_group_ids = [var.sg_id]
   tags = {
     Name = var.component
   }
